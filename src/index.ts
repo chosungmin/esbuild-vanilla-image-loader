cc37aa4ec4b61c7b682510d47be61bcc7c04c74e
조성민 <chosungmin.kr@navercorp.com>
2025-04-25 13:47:18 +0900

    file 타입 추가

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,67 @@
-import fs from 'fs'
 import type { Plugin } from 'esbuild'
+import fs from 'fs/promises';
+import path from 'path';
+import hasha  from 'hasha';
 
 interface Options {
   filter?: RegExp
   dataUrl?: false
+  file?: false
+  outputPath?: string
 }
 
-export function ImageLoader(options?: Options): Plugin {
+export function ImageLoader({outputPath='dist/assets', ...options}: Options): Plugin {
   return {
     name: 'esbuild-vanilla-image-loader',
     setup(build) {
       build.onLoad({
         filter: options?.filter ?? /\.(gif|jpe?g|tiff?|png|webp|bmp|svg)$/,
-      }, (args) => {
+      }, async (args) => {
         if (options?.dataUrl) {
           return {
-            contents: fs.readFileSync(args.path),
+            contents: await fs.readFile(args.path),
             loader: 'dataurl',
           }
         }
 
-        return {
-          contents: `
-          const file = '${args.path.replace(build.initialOptions.absWorkingDir!, '')}';
-          export default file;`,
-          loader: 'js',
+        if (options?.file) {
+          return {
+            contents: `
+            const file = '${args.path.replace(build.initialOptions.absWorkingDir!, '')}';
+            export default file;`,
+            loader: 'js',
+          }
         }
+
+        const fileContent =  await fs.readFile(args.path);
+
+        // 1. 해시 계산
+        const hash = await hasha(fileContent, { algorithm: 'sha1' });
+        const ext = path.extname(args.path);
+        const base = path.basename(args.path, ext);
+        const hashedName = `${base}-${hash.slice(0, 16)}${ext}`;
+
+        const outPath = path.resolve(outputPath, hashedName);
+        
+        // 2. 파일 존재 여부 확인
+        let alreadyExists = false;
+        try {
+          await fs.access(outPath);
+          alreadyExists = true;
+        } catch {
+          alreadyExists = false;
+        }
+
+        // 3. 파일이 없을 경우 복사
+        if (!alreadyExists) {
+          await fs.mkdir(path.dirname(outPath), { recursive: true });
+          await fs.writeFile(outPath, fileContent);
+        }
+      
+        return {
+          contents: `/assets/${hashedName}`,
+          loader: "text"
+        };
       })
     },
   }
